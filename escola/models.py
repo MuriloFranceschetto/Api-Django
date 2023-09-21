70c7aaadf766e5bb663164a32cb9107bb48fc35c
Murilo Franceschetto <gfmurilogf@gmail.com>
2023-09-21 20:50:51 -0300

    Documentos dos alunos únicos

--- escola/models.py
+++ escola/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=30)
-    rg = models.CharField(max_length=9)
-    cpf = models.CharField(max_length=11)
+    rg = models.CharField(max_length=9, unique=True)
+    cpf = models.CharField(max_length=11, unique=True)
     data_nascimento = models.DateField()
 
     def __str__(self):
@@ -17,7 +17,7 @@ class Curso(models.Model):
         ('I', 'Intermediário'),
         ('A', 'Avançado'),
     )
-    codigo_curso = models.CharField(max_length=10)
+    codigo_curso = models.CharField(max_length=10, unique=True)
     descricao = models.CharField(max_length=100)
     nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
 
